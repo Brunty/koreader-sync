8931501a3ac78cc0a485e461e5a10fceb7fb59a8
Matt Brunt <matt@brunty.me>
2026-05-10 20:56:44 +0100

    Add initial crypto test

---
+++ crypto/password_test.go
@@ -0,0 +1,15 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPasswordHashing(t *testing.T) {
+	password := "password123" // so secure amirite?!
+	hashedPassword, err := HashPassword(password)
+
+	assert.NoErrorf(t, err, "Should have no error from hashing the crypto")
+	assert.True(t, CheckPasswordHash(password, hashedPassword))
+}
