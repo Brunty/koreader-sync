5818ebe3a334a3c532fd93be350530ecb357bf0c
Matt Brunt <matt@brunty.me>
2026-05-12 18:38:52 +0100

    Add tests

--- crypto/password_test.go
+++ crypto/password_test.go
@@ -13,3 +13,21 @@ func TestPasswordHashing(t *testing.T) {
 	assert.NoErrorf(t, err, "Should have no error from hashing the crypto")
 	assert.True(t, BcryptCheckPasswordHash(password, hashedPassword))
 }
+
+func TestBcryptNeedsRehash(t *testing.T) {
+	// Has a cost of 4
+	oldHashedPw := "$2y$04$S4sNf81050H0lCz8v3Hfh.tnKtTJGoZ63Z4qUCL//f284CS5i7d8q"
+
+	// Checks against BcryptCost which wouldn't be set to 4
+	assert.True(t, BcryptNeedsRehash(oldHashedPw))
+}
+
+func TestBcryptDoesNotNeedRehash(t *testing.T) {
+	newHashedPassword, _ := BcryptHashPassword("password")
+
+	assert.False(t, BcryptNeedsRehash(newHashedPassword))
+}
+
+func TestBcryptDoesNotNeedRehashIfHashError(t *testing.T) {
+	assert.False(t, BcryptNeedsRehash("something bad here"))
+}
