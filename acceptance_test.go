92f9a8c1f7a4316783a2e8252bd4e460d20862e4
Matt Brunt <matt@brunty.me>
2026-05-14 20:37:25 +0100

    Add acceptance tests for key user flows

--- acceptance_test.go
+++ acceptance_test.go
@@ -10,13 +10,18 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/brunty/koreader-sync-server/crypto"
 	"github.com/brunty/koreader-sync-server/db"
+	userpackage "github.com/brunty/koreader-sync-server/user"
 	"github.com/stretchr/testify/assert"
 )
 
 // These tests are more like acceptance tests as I want to call via HTTP so that it's not just invoking handlers, that
 // way we can check things like auth middleware works properly on appropriate routes and the whole flows work
+// These aren't testing full logic of the handlers (the handler tests are better for that) these are to ensure that
+// things work within the HTTP context and setting the user on the request context is handled correctly
 
 func setupAcceptanceTests(t *testing.T) *httptest.Server {
 	t.Cleanup(func() {
@@ -34,12 +39,12 @@ func setupAcceptanceTests(t *testing.T) *httptest.Server {
 	return httptest.NewServer(mux)
 }
 
-func marshalResponseBodyToMap(t *testing.T, rsp *http.Response) map[string]string {
+func marshalResponseBodyToMap(t *testing.T, rsp *http.Response) map[string]any {
 	body, err := io.ReadAll(rsp.Body)
 	slog.Debug(string(body))
 	assert.NoError(t, err)
 
-	var result map[string]string
+	var result map[string]any
 	err = json.Unmarshal(body, &result)
 	assert.NoError(t, err)
 
@@ -102,8 +107,8 @@ func TestUnauthorizedEndpoints(t *testing.T) {
 		},
 	}
 
-	for testName, tt := range endpoints {
-		t.Run(testName, func(t *testing.T) {
+	for desc, tt := range endpoints {
+		t.Run(desc, func(t *testing.T) {
 			req, _ := http.NewRequest(tt.method, fmt.Sprintf("%s%s", server.URL, tt.url), nil)
 			authRsp, err := client.Do(req)
 			assert.NoError(t, err)
@@ -122,7 +127,7 @@ func TestUserCreateAndAuthFlow(t *testing.T) {
 
 	client := &http.Client{}
 
-	createBody := `{"username":"test-user","password":"test- pass"}`
+	createBody := `{"username":"test-user","password":"test-pass"}`
 	createReq, _ := http.NewRequest("POST", server.URL+"/users/create", strings.NewReader(createBody))
 	createRsp, err := client.Do(createReq)
 	assert.NoError(t, err)
@@ -130,11 +135,53 @@ func TestUserCreateAndAuthFlow(t *testing.T) {
 
 	authReq, _ := http.NewRequest("GET", server.URL+"/users/auth", nil)
 	authReq.Header.Set("x-auth-user", "test-user")
-	authReq.Header.Set("x-auth-key", "test- pass")
+	authReq.Header.Set("x-auth-key", "test-pass")
 	authRsp, err := client.Do(authReq)
 	assert.NoError(t, err)
 	assert.Equal(t, http.StatusOK, authRsp.StatusCode)
 
 	body := marshalResponseBodyToMap(t, authRsp)
 	assert.Equal(t, "authorized", body["status"])
+}
+
+func TestUserSyncProgressFlow(t *testing.T) {
+	server := setupAcceptanceTests(t)
+	defer server.Close()
+
+	repo := userpackage.NewUserRepository(db.DBCon)
+
+	password, _ := crypto.BcryptHashPassword("test-pass")
+
+	user := userpackage.User{
+		Username:  "test-user",
+		Password:  password,
+		CreatedAt: time.Now(),
+	}
+
+	_, err := repo.Store(t.Context(), user)
+
+	client := &http.Client{}
+
+	syncProgressBody := `{"device_id":"my-device","progress":"/some/progress[2]/here","document":"super-book","percentage":0.123,"device":"my-reader"}`
+	createReq, _ := http.NewRequest("PUT", server.URL+"/syncs/progress", strings.NewReader(syncProgressBody))
+	createReq.Header.Set("x-auth-user", "test-user")
+	createReq.Header.Set("x-auth-key", "test-pass")
+	createRsp, err := client.Do(createReq)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, createRsp.StatusCode)
+
+	getSyncProgressReq, _ := http.NewRequest("GET", server.URL+"/syncs/progress/super-book", nil)
+	getSyncProgressReq.Header.Set("x-auth-user", "test-user")
+	getSyncProgressReq.Header.Set("x-auth-key", "test-pass")
+	authRsp, err := client.Do(getSyncProgressReq)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, authRsp.StatusCode)
+
+	body := marshalResponseBodyToMap(t, authRsp)
+	assert.Equal(t, "my-device", body["device_id"])
+	assert.Equal(t, "/some/progress[2]/here", body["progress"])
+	assert.Equal(t, "super-book", body["document"])
+	assert.Equal(t, 0.123, body["percentage"])
+	assert.Equal(t, "my-reader", body["device"])
+	assert.NotEqual(t, 0, body["timestamp"])
 }
